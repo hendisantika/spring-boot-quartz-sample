59211dbdf361e619ec352a11b3b6b67b9c368d53
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 10:56:45 +0700

    Add MailService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/quartzsample/service/MailService.java
@@ -0,0 +1,42 @@
+package id.my.hendisantika.quartzsample.service;
+
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quartz-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/24
+ * Time: 10.56
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender sender;
+
+    public void send(String from, String recipient, String subject, String body) {
+        try {
+            log.info("Sending mail to {}", recipient);
+            MimeMessage message = sender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom(new InternetAddress(from));
+            helper.setTo(recipient);
+            helper.setSubject(subject);
+            helper.setText(body);
+            sender.send(message);
+        } catch (Exception e) {
+            log.info(e.toString());
+        }
+    }
+}
