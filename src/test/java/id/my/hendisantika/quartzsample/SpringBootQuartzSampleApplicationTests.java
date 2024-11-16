336e5af7fd9113db1d9b2ed46a5505939b2161d0
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 11:26:17 +0700

    Add Test Container Step🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/quartzsample/SpringBootQuartzSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/quartzsample/SpringBootQuartzSampleApplicationTests.java
@@ -1,13 +1,29 @@
 package id.my.hendisantika.quartzsample;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.quartzsample.repository.AuthorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mysql:9.1.0:///postsDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootQuartzSampleApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        authorRepository.deleteAll();
     }
 
 }
