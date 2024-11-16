5b0d7187fc8950b3a7054d9d46ac43dd145736db
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 10:42:58 +0700

    Add handleUnauthorizedException in BaseControllerAdvice🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/quartzsample/exception/base/BaseControllerAdvice.java
+++ src/main/java/id/my/hendisantika/quartzsample/exception/base/BaseControllerAdvice.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.quartzsample.exception.base;
 import id.my.hendisantika.quartzsample.exception.BadRequestException;
 import id.my.hendisantika.quartzsample.exception.DataNotFoundException;
 import id.my.hendisantika.quartzsample.exception.DuplicateException;
+import id.my.hendisantika.quartzsample.exception.UnauthorizedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
@@ -52,5 +53,12 @@ public class BaseControllerAdvice {
     public ErrorResponse handleBadRequestException(Exception ex) {
         return new ErrorResponse(
                 String.valueOf(HttpStatus.BAD_REQUEST.value()), ex.getMessage(), TIMESTAMP);
+    }
+
+    @ExceptionHandler({UnauthorizedException.class})
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleUnauthorizedException(Exception ex) {
+        return new ErrorResponse(
+                String.valueOf(HttpStatus.UNAUTHORIZED.value()), ex.getMessage(), TIMESTAMP);
     }
 }
