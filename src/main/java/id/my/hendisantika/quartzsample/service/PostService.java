069ec0361d2f2485a12617bcac0d532f2f9adc87
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 10:57:20 +0700

    Add PostService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/quartzsample/service/PostService.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.quartzsample.service;
+
+import id.my.hendisantika.quartzsample.repository.AuthorRepository;
+import id.my.hendisantika.quartzsample.repository.PostRepository;
+import id.my.hendisantika.quartzsample.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quartz-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/24
+ * Time: 10.57
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PostService {
+
+    private final ModelMapper modelMapper;
+
+    private final PostRepository postRepository;
+
+    private final AuthorRepository authorRepository;
+
+    private final TagRepository tagRepository;
+}
