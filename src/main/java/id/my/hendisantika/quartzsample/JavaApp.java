48b2c49812e8307792db01c2fee8ae22c3e178cd
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 11:01:58 +0700

    Add executeInternal in Job🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/quartzsample/JavaApp.java
+++ src/main/java/id/my/hendisantika/quartzsample/JavaApp.java
@@ -2,6 +2,7 @@ package id.my.hendisantika.quartzsample;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.quartzsample.model.dto.JobDTO;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
--- src/main/java/id/my/hendisantika/quartzsample/job/Job.java
+++ src/main/java/id/my/hendisantika/quartzsample/job/Job.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.quartzsample.job;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.quartzsample.model.dto.JobDTO;
+import id.my.hendisantika.quartzsample.service.MailService;
 import io.micrometer.common.lang.NonNullApi;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
@@ -30,5 +35,22 @@ public class Job extends QuartzJobBean {
     @Autowired
     public void autowire(final MailService mailService) {
         this.mailService = mailService;
+    }
+
+    @Override
+    public void executeInternal(final JobExecutionContext context) {
+        log.info("Job execute {}", context.getJobDetail().getKey().getName());
+        JobDTO jobDTO = null;
+        try {
+            jobDTO =
+                    new ObjectMapper()
+                            .readValue(context.getMergedJobDataMap().getString("jobDTO"), JobDTO.class);
+        } catch (JsonProcessingException e) {
+            log.info(e.toString());
+        }
+        mailService.send(jobDTO.getFrom(), jobDTO.getTo(), jobDTO.getSubject(), jobDTO.getBody());
+        if (false) {
+            this.unScheduleJob(context);
+        }
     }
 }
