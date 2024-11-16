6067a806ab6f4a72d3cfb65db3cd364c8f5219ef
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 10:22:59 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/quartzsample/SpringBootQuartzSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.quartzsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuartzSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuartzSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/quartzsample/SpringBootQuartzSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.quartzsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuartzSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
