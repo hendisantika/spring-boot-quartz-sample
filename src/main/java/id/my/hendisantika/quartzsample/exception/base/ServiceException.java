2991b594507edb63ab192562bed6e75ee4546edb
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 10:37:30 +0700

    Add ServiceException🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/quartzsample/exception/base/ServiceException.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.quartzsample.exception.base;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quartz-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/24
+ * Time: 10.37
+ * To change this template use File | Settings | File Templates.
+ */
+@SuppressWarnings("serial")
+public class ServiceException extends RuntimeException {
+
+    public ServiceException() {
+        super();
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public ServiceException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
