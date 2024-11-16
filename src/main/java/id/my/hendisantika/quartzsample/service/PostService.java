65f8550af193d1c3e342e792f3409753c4432aa2
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 10:58:07 +0700

    Add createOrUpdate in PostService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/quartzsample/service/PostService.java
+++ src/main/java/id/my/hendisantika/quartzsample/service/PostService.java
@@ -1,6 +1,8 @@
 package id.my.hendisantika.quartzsample.service;
 
 import id.my.hendisantika.quartzsample.exception.DataNotFoundException;
+import id.my.hendisantika.quartzsample.model.dto.PostDTO;
+import id.my.hendisantika.quartzsample.model.entity.Author;
 import id.my.hendisantika.quartzsample.model.entity.Post;
 import id.my.hendisantika.quartzsample.repository.AuthorRepository;
 import id.my.hendisantika.quartzsample.repository.PostRepository;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -55,6 +58,25 @@ public class PostService {
                         () ->
                                 new DataNotFoundException(
                                         MessageFormat.format("Post id {0} not found", String.valueOf(id))));
+    }
+
+    public Post createOrUpdate(PostDTO postRequest) {
+        Optional<Post> existingPost = postRepository.findById(postRequest.getId());
+
+        if (existingPost.isPresent()) {
+            Post postUpdate = existingPost.get();
+
+            postUpdate.setTitle(postRequest.getTitle());
+            postUpdate.setBody(postRequest.getBody());
+            if (postRequest.getAuthorId() != 0) {
+                Optional<Author> author = authorRepository.findById(postRequest.getAuthorId());
+                author.ifPresent(postUpdate::setAuthor);
+            }
+
+            return postRepository.save(postUpdate);
+        } else {
+            return postRepository.save(modelMapper.map(postRequest, Post.class));
+        }
     }
 
 }
