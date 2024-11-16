fe84f0dc9aacaf256d931e0903e2446a5b57d0d5
hendisantika <hendisantika@yahoo.co.id>
2024-11-16 11:13:01 +0700

    Add createOrUpdate in AuthorController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/quartzsample/controller/AuthorController.java
+++ src/main/java/id/my/hendisantika/quartzsample/controller/AuthorController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -40,4 +42,11 @@ public class AuthorController {
         Author entity = service.getById(id);
         return new ResponseEntity<>(entity, HttpStatus.OK);
     }
+
+    @PostMapping("/v1/authors")
+    public ResponseEntity<Author> createOrUpdate(@RequestBody Author Author) {
+        Author updated = service.createOrUpdate(Author);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+
 }
